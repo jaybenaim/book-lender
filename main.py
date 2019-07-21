e9b2eb4994342f4ec9537af1f0158eb75b0441b5
jaybenaim <benaimjacob@gmail.com>
2019-07-21 00:40:58 -0400

    search method created. Stretch complete

--- main.py
+++ main.py
@@ -8,21 +8,24 @@ class Book:
     day = []
     borrowed = False
     genres = []
-    def __init__(self, title, author, genre, ISBN):
+    all_books = []
+
+    def __init__(self, title, author, genre, isbn):
         self.title = title
         self.author = author
-        self.ISBN = ISBN
+        self.isbn = isbn
 
     def __str__(self):
-        return f"{self.title} by {self.author}, ISBN: {self.ISBN} "
+        return f"{self.title} by {self.author}, isbn: {self.isbn} "
 
     @classmethod
     # instantiate a Book
-    def create(cls, title, author, genre, ISBN):
+    def create(cls, title, author, genre, isbn):
         """ this will create a new book """
-        new_book = Book(title, author, genre, ISBN)
+        new_book = Book(title, author, genre, isbn)
         cls.on_shelf.append(new_book)
         cls.genres.append(genre)
+        cls.all_books.append(new_book)
         return new_book
 
     @classmethod
@@ -57,11 +60,39 @@ class Book:
             return random_book
         else:
             return "There are no books on the shelf, come back tomorrow"
+
     @classmethod
-    def browse_genres(cls): 
+    def browse_genres(cls):
         if cls.genres != []:
             random_genre = random.choice(cls.genres)
             return random_genre
+
+    @classmethod
+    def search(cls, title, author, isbn):
+        for book_title in cls.all_books:
+            if book_title.title == title:
+                return f"Yes we carry {book_title}"
+            elif book_title.author == author:
+                return f"Yes we carry {book_title}"
+            elif book_title.isbn == isbn:
+                return f"Yes we carry {book_title}"
+            else:
+                return f"Sorry, we dont carry that."
+
+    # @classmethod
+    # def search_author(cls, author)::
+    #     for book_title in cls.all_books:
+    #         if book_title.title == title:
+    #             return f'Yes we carry {book_title}'
+    #         else:
+    #             return f'Sorry, we dont carry that.'
+    # @classmethod
+    # def search_isbn(cls, isbn):
+    #     for book_title in cls.all_books:
+    #         if book_title.title == title:
+    #             return f'Yes we carry {book_title}'
+    #         else:
+    #             return f'Sorry, we dont carry that.'
 
     def borrow(self):
         """ This instance method is how a book is taken out of the library.
@@ -70,7 +101,7 @@ class Book:
         to borrow the book failed. Otherwise, use current_due_date to set the due_date
         of the book and move it from the collection 
         of available books to the collection of books on loan, then return True. """
-        
+
         if self.lent_out() == True:
             return f"The Book is already out on loan."
         elif self.lent_out() == False:
@@ -104,7 +135,7 @@ class Book:
     @classmethod
     def renew(cls):
 
-        if cls.borrowed == False: 
+        if cls.borrowed == False:
             now = datetime.now()
             two_weeks = 60 * 60 * 24 * 14  # two weeks expressed in seconds
             future_timestamp = now.timestamp() + two_weeks * 2
@@ -114,17 +145,20 @@ class Book:
             cls.string = f" New Due Date {month}/{day}/{year}"
             Book.borrowed = True
             return cls.string
-        if cls.borrowed == True: 
-            return 'You cannot renew this book again'
-        
+        if cls.borrowed == True:
+            return "You cannot renew this book again"
 
 
-sister_outsider = Book.create("Sister Outsider", "Audre Lorde", 'fiction' , "9781515905431")
-aint_i = Book.create("Ain't I a Woman?", "Bell Hooks", 'fiction', "9780896081307")
-if_they_come = Book.create("If They Come in the Morning", "Angela Y. Davis", 'non-fiction', "0893880221")
+sister_outsider = Book.create(
+    "Sister Outsider", "Audre Lorde", "fiction", "9781515905431"
+)
+aint_i = Book.create("Ain't I a Woman?", "Bell Hooks", "fiction", "9780896081307")
+if_they_come = Book.create(
+    "If They Come in the Morning", "Angela Y. Davis", "non-fiction", "0893880221"
+)
 
 
-print(Book.browse().title)
+# print(Book.browse().title)
 # Book.browse().title  # "Ain't I a Woman?" (this value may be different for you)
 # len(Book.on_shelf)  # 3
 # len(Book.on_loan)  # 0
@@ -135,12 +169,14 @@ print(Book.browse().title)
 # sister_outsider.lent_out()  # True
 # print(sister_outsider.borrow())  # False
 # print(sister_outsider.current_due_date())
-# print(sister_outsider.renew())  
 # print(sister_outsider.renew())
-print(Book.browse_genres())
+# print(sister_outsider.renew())
+# print(Book.browse_genres())
 # print(sister_outsider.current_due_date())
 # print(len(Book.overdue_books()))  # 0
 # print(sister_outsider.return_to_library())  # True
 # print(sister_outsider.lent_out())  # False
 # print(len(Book.on_shelf))  # 3
 # print(len(Book.on_loan))  # 0
+print(Book.search(None, "Audre Lorde", None))  # fuzzy search ?
+print(Book.search("Sister Outsidersas", None, None))
